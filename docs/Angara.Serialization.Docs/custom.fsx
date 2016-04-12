8aff1c6fc62ec63b628f35e291205a819384f3cd
sberezin <s_berezin@cs.msu.su>
2016-04-12 19:33:37 +0300

    Initial documentation drop.

---
+++ docs/Angara.Serialization.Docs/custom.fsx
@@ -0,0 +1,77 @@
+﻿
+(** 
+###Serializers, Resolvers, Libraries and Typeids *)
+
+(** The Serialization service uses textual identifier (typeid) to differentiate types of objects in the 
+serializable representation. Primary benefit of this approach is independence of serializable 
+representation of .NET platform. Also type ids can be chosen to be shorter than .NET full type 
+names especially in case of generic types.
+
+Current implementation needs to know type id and serialization function in order to convert an 
+artefact into serializable representation and it needs deserialization function for type id 
+in order to restore artefact from serializable representation.
+
+SerializerResolvers are responsible for returning serializer for given CLR Type or typeid.
+
+SerializerLibrary is a SerializerResolver that can looks among serializers added by user. *)
+
+(**
+###Creating custom serializer
+
+Let's build and use serializer for `Vector2d` type *)
+
+type Vector2d = { x: double; y: double }
+
+(**
+
+Serializable artefacts can be converted to `InfoSet` object 
+and restored from 'InfoSet'. 
+
+Artefacts of type 'T are serialized and deserialized by some object implementing type ISerializer<'T>. 
+Serializer types has serialization and deserializationb method and property for Type ID. 
+*)
+
+type Vector2dSerializer () =
+    interface ISerializer<Vector2d> with
+        member x.TypeId = "V2D"
+        member x.Serialize _ v = 
+            InfoSet.EmptyMap.AddDouble("x", v.x).AddDouble("y", v.y)
+        member x.Deserialize _ v = 
+            let map = InfoSet.toMap v in { x = map.["x"].ToDouble(); y = map.["y"].ToDouble() } 
+
+(** We need to let Serialization infrastructure to know about serializer for `Vector2d`. To do this
+we create library, add our serializer to library and use it in invocations of serialization methods 
+
+`CreateDefault` method creates serializer library which has all core types in it*)
+
+let res = SerializerLibrary.CreateDefault()
+res.Register(Vector2dSerializer())
+
+let v = { x = -1.0; y = 1.0 }
+let vjson = Json.FromObject(res, v)
+
+(** Resulting JSON is *)
+
+(*** include-value: vjson.ToString() ***)
+
+(** 
+###Access to serialization infrastructure inside serializers
+
+Next example describes first argument of the serializer/deserializer methods.
+*)
+
+type MapSerializer () = 
+    interface ISerializer<Map<string, obj>> with
+        member x.TypeId = "Map"
+        member x.Serialize resolver obj = 
+            obj |> Map.fold (fun infoSet key value -> 
+                infoSet.AddInfoSet(key, ArtefactSerializer.Serialize resolver value)) InfoSet.EmptyMap
+        member x.Deserialize resolver infoSet = 
+            infoSet.ToMap() |> Map.map (fun _ value -> ArtefactSerializer.Deserialize resolver value)
+
+let map = Map.empty<string,obj>.Add("Pi", 3.1415 :> obj).Add("Vector", { x = 10.0; y = -20.0 } :> obj)
+res.Register(MapSerializer())
+let mjson = Json.FromObject(res, map)
+
+(*** include-value: mjson.ToString() ***)
+
---
+++ docs/Angara.Serialization.Docs/generate.fsx
@@ -0,0 +1,72 @@
+﻿#I "../../ext/nuget/FSharp.Formatting.CommandTool.2.14.1/tools"
+#r "../../ext/nuget/FSharp.Formatting.CommandTool.2.14.1/tools/FSharp.CodeFormat.dll"
+#r "../../ext/nuget/FSharp.Formatting.CommandTool.2.14.1/tools/FSharp.Literate.dll"
+#r "../../ext/nuget/FSharp.Formatting.CommandTool.2.14.1/tools/FSharp.Markdown.dll"
+#r "../../ext/nuget/FSharp.Formatting.CommandTool.2.14.1/tools/FSharp.MetadataFormat.dll"
+
+open FSharp.Literate
+open FSharp.MetadataFormat
+open System.IO
+
+let info = [ "project-name", "Angara.Serialization"
+           ; "project-summary", "A library that facilitates serialization on both .NET and JavaScript platforms and communication between them"
+           ; "project-author", "Microsoft Research" 
+           ; "root", "." 
+           ; "project-nuget", "https://www.nuget.org/packages?q=Angara.Serialization"
+           ; "project-github", "https://github.com/Microsoft/Angara.Serialization"]
+
+Directory.SetCurrentDirectory(__SOURCE_DIRECTORY__)
+let templates = [ Path.GetFullPath "../templates"; Path.GetFullPath "../templates/reference" ]
+let docTemplate = "docpage.cshtml"
+let tips_path = Path.GetFullPath "../../ext/nuget/FSharp.Formatting.CommandTool.2.14.1/styles/tips.js"
+let style_path = Path.GetFullPath "../../ext/nuget/FSharp.Formatting.CommandTool.2.14.1/styles/style.css"
+let output_dir = Path.GetFullPath "../out"
+let output path = Path.Combine(output_dir, Path.GetFileName path)
+let content path = Path.Combine(output_dir, (Path.Combine("content", Path.GetFileName path)))
+let src path = Path.Combine(Path.GetFullPath "../../docs/Angara.Serialization.Docs", path)
+
+
+// prepare output directory
+if Directory.Exists(output "") then Directory.Delete(output "", true)
+Directory.CreateDirectory(output "")
+Directory.CreateDirectory(content "")
+for f in [tips_path; style_path] do
+    printfn "%s" (content f)
+    File.WriteAllText(content f, File.ReadAllText f)
+
+// Set current directory to templates
+Directory.SetCurrentDirectory("../templates")
+printfn "Current directory is %s" (Directory.GetCurrentDirectory())
+
+// Generates HTML from FSharp script files using F# Literate Programming.
+// process script files
+
+let fsi = FsiEvaluator()
+for f in 
+    [ src "serialization.fsx" ] 
+    do
+    if File.Exists f then
+        printfn "Processing %s" (Path.GetFullPath(f))
+        Literate.ProcessScriptFile(f, 
+                                   docTemplate, 
+                                   Path.ChangeExtension(output f, "html"), 
+                                   fsiEvaluator=fsi, 
+                                   replacements = info,
+                                   layoutRoots = [ Path.Combine(__SOURCE_DIRECTORY__, "../templates") ])
+    else printfn "No such file: %s" f
+
+// Generates F# library documentation from inline comments.
+//for assembly in [  src "bin/Debug/Angara.Serialization.dll"
+//                   ]
+//    do
+//    if File.Exists assembly then
+//        printfn "Processing %s" (Path.GetFullPath(assembly))
+//        MetadataFormat.Generate 
+//            ( assembly,
+//              output "",
+//              templates,
+//              sourceRepo = "https://github.com/Microsoft/Angara.Serialization/tree/master",
+//              sourceFolder = Path.GetFullPath(assembly),
+//              markDownComments = true,
+//              parameters = info)
+//    else printfn "No such file: %s" assembly  
